ff787f046d3e93a5bf6703e04b069fb37883d83e
cc123455478675 <114322974+cc123455478675@users.noreply.github.com>
2022-09-25 14:31:59 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -65,7 +65,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小美女',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'onjIU54omfYK3uh7Hb9efRNPUfsE',
+      id: 'onjIU5-yjQCf2fAxnWipHc425V-M',
       // 你想对他发送的模板消息的模板ID
       useTemplateId: 'y846xkoC52VhVtBqXapWNwlJEcqc6cDtb5UAnMZmG00',
       // 所在省份
@@ -171,11 +171,11 @@ const USER_CONFIG = {
       },
     },
     {
-      name: '老婆1',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
+      name: '小美女1',
+      id: 'onjIU54omfYK3uh7Hb9efRNPUfsE',
+      useTemplateId: 'y846xkoC52VhVtBqXapWNwlJEcqc6cDtb5UAnMZmG00',
+      province: '山东',
+      city: '青岛',
       horoscopeDate: '',
       horoscopeDateType: '',
       openUrl: 'https://wangxinleo.cn',
@@ -218,7 +218,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'sEbRxZxv05oaMKE3pYvABCKLoI3Y4_W1eelslVbPtHw',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -226,7 +226,7 @@ const USER_CONFIG = {
       // 一般都填自己
       name: '自己',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'gh_9f64d8e28bd7',
     }
     // 你可以不断按格式往下增加
     // ...
@@ -239,9 +239,9 @@ const USER_CONFIG = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '广东',
+  PROVINCE: '山东',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '惠州',
+  CITY: '青岛',
 
   /** 重要节日相关 */
 
